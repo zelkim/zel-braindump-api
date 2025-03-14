b197e53fea965b24ef03f1bba663846112dae422
zelkim <dev86.sean@gmail.com>
2025-03-14 12:57:49 +0800

    feat: views

--- models/Article.js
+++ models/Article.js
@@ -8,6 +8,13 @@ const articleSchema = new mongoose.Schema({
     imageurl: { type: String, required: false },
     content: { type: String, required: true },
     created_at: { type: Date, required: true, default: Date.now },
+    views: { type: Number, default: 0 },
+});
+
+articleSchema.post("findOne", async (doc) => {
+    if (doc) {
+        await doc.updateOne({ $inc: { views: 1 } });
+    }
 });
 
 export default mongoose.model("articles", articleSchema);
